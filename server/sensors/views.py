405631f8f6c811af341482301edba630ecb26ee7
void <piotr.stankiewicz@gmail.com>
2020-10-05 05:09:57 +0200

    fixed sensor data filtering

--- server/sensors/views.py
+++ server/sensors/views.py
@@ -17,6 +17,13 @@ class SensorDataViewSet(viewsets.ModelViewSet):
         "post",
     ]
 
+    def get_queryset(self):
+        queryset = SensorData.objects.all().order_by("datetime")
+        sensor_uuid = self.request.query_params.get("sensor", None)
+        if sensor_uuid:
+            queryset = queryset.filter(sensor__uuid=sensor_uuid)
+        return queryset
+
 
 class SensorDetail(DetailView):
 
